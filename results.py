0a979a8697e04e2e4e3b5050f874ac28d6ed154f
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-10-02 00:47:09 +0530

    Add files via upload

---
+++ results.py
@@ -0,0 +1,33 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import pickle
+import re
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import nltk
+nltk.data.path.append('nltk_data')
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+
+def preprocess(text, stem=False):
+  text = re.sub("@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+", ' ', str(text).lower()).strip()
+  stop_words = stopwords.words('english')
+  stemmer = SnowballStemmer('english')
+  tokens = []
+  for token in text.split():
+    if token not in stop_words:
+      if stem:
+        tokens.append(stemmer.stem(token))
+      else:
+        tokens.append(token)
+  return " ".join(tokens)
+
+inp = input()
+inp = preprocess(inp)
+vect = pickle.load(open("vectorizer.pickle",'rb'))
+inp = vect.transform([inp]).toarray().reshape(1,1,2500)
+mod = tf.keras.models.load_model('model.h5')
+out = mod.predict(inp)
+print(out[0][0])
