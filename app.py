961e2eacff8473c569c3e5696005d26b7fa123a3
Mohil Patel <mohilpatel21@gmail.com>
2024-09-18 21:12:13 +0530

    refactor code

--- app.py
+++ app.py
@@ -1,43 +1,54 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow as tf
+import pickle
+
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+from flask import request
+import numpy as np
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import re
+
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+import tensorflow as tf
 
 app = Flask(__name__)
-vect = pickle.load(open("vectorizer.pickle",'rb'))
-model = tf.keras.models.load_model('model.h5')
+vect = pickle.load(open("vectorizer.pickle", "rb"))
+model = tf.keras.models.load_model("model.h5")
+
 
 def preprocess(text, stem=False):
-  text = re.sub("@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+", ' ', str(text).lower()).strip()
-  stop_words = stopwords.words('english')
-  stemmer = SnowballStemmer('english')
-  tokens = []
-  for token in text.split():
-    if token not in stop_words:
-      if stem:
-        tokens.append(stemmer.stem(token))
-      else:
-        tokens.append(token)
-  return " ".join(tokens)
+    text = re.sub(
+        "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+", " ", str(text).lower()
+    ).strip()
+    stop_words = stopwords.words("english")
+    stemmer = SnowballStemmer("english")
+    tokens = []
+    for token in text.split():
+        if token not in stop_words:
+            if stem:
+                tokens.append(stemmer.stem(token))
+            else:
+                tokens.append(token)
+    return " ".join(tokens)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     inp = [x for x in request.form.values()]
     inp = preprocess(inp)
-    inp = vect.transform([inp]).toarray().reshape(1,1,2500)
+    inp = vect.transform([inp]).toarray().reshape(1, 1, 2500)
     output = model.predict(inp)[0]
 
-    return render_template('index.html', prediction_text='{}'.format(output[0]))
+    return render_template("index.html", prediction_text="{}".format(output[0]))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
