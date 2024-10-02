082094a4e5def03ac687330e69cfe02ee94a0a81
Mohil Patel <mohilpatel21@gmail.com>
2024-10-02 18:06:48 +0530

    Add styling and streamlit app

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Sentiment Analysis
+
 A simple sentiment analysis application made using following packages:
+
 - Tensorflow Keras
 - Natural Language Toolkit (NLTK)
 - TfidfVectorizer
 
-
 The model was trained using tweets from [Sentiment140 dataset with 1.6 million tweets](http://www.kaggle.com/kazanova/sentiment140).
 
-The app has has been deployed using Flask and Heroku. The app can be found [here](http://sentiment-analysis-mp.herokuapp.com/).
+The UI has has been deployed Streamlit
--- app.py
+++ app.py
@@ -1,54 +1,107 @@
-import os
+"""Streamlit Sentiment Analysis App."""
+
+import html
 import pickle
-
-from flask import Flask
-from flask import jsonify
-from flask import render_template
-from flask import request
-import numpy as np
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import re
 
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
+import streamlit as st
 import tensorflow as tf
-
-app = Flask(__name__)
-vect = pickle.load(open("vectorizer.pickle", "rb"))
-model = tf.keras.models.load_model("model.h5")
+from nltk.corpus import stopwords
 
 
-def preprocess(text, stem=False):
+def set_session() -> None:
+    """Set session variables."""
+
+    if "vectorizer" not in st.session_state:
+        with open("vectorizer.pickle", "rb") as f:
+            st.session_state.vectorizer = pickle.load(f)
+    if "model" not in st.session_state:
+        st.session_state.model = tf.keras.models.load_model("model.keras")
+
+
+def preprocess(text: str) -> str:
+    """Preprocess text.
+
+    Args:
+        text (str): text to be processed.
+
+    Returns:
+        str: Processed text.
+    """
+
+    text = text.lower()  # lower the text
+    text = html.unescape(text)  # parse html entitities
     text = re.sub(
-        "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+", " ", str(text).lower()
-    ).strip()
+        r"@\S+|https?:\S+|[^A-Za-z0-9]+", " ", text
+    ).strip()  # remove the unwanted text
     stop_words = stopwords.words("english")
-    stemmer = SnowballStemmer("english")
-    tokens = []
-    for token in text.split():
-        if token not in stop_words:
-            if stem:
-                tokens.append(stemmer.stem(token))
-            else:
-                tokens.append(token)
-    return " ".join(tokens)
+    tokens = [token for token in text.split() if token not in stop_words]
+    text = " ".join(tokens)
+    return text
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def main() -> None:
+    """Main runner."""
 
+    set_session()
+    st.header("Sentiment Analyser")
+    input_text = st.text_area(label="Input text")
+    button = st.button(label="Analyse")
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    inp = [x for x in request.form.values()]
-    inp = preprocess(inp)
-    inp = vect.transform([inp]).toarray().reshape(1, 1, 2500)
-    output = model.predict(inp)[0]
+    progress_bar_value = """
+    <div style="
+            width: 100%;
+            height: 30px;
+            background-color: #e0e0e0;
+            border-radius: 5px;
+            overflow: hidden;
+            margin-bottom: 20px">
+        <div style="
+            height: 100%;
+            width: {}%;
+            background-color: #3371ff;
+            border-radius: 5px;
+            transition: width 2s ease-in-out;">
+        </div>
+    </div>
+    """
+    progress_bar = st.markdown(progress_bar_value.format(0), unsafe_allow_html=True)
+    percentage = 50
+    progress_style = """
+            <div style="
+                display: flex;
+                justify-content: space-between;
+                align-items: center;">
+                <span>Negative</span>
+                <span>{}</span>
+                <span>Positive</span>
+            </div>
+            """
+    progress_text = st.markdown(
+        progress_style.format(percentage), unsafe_allow_html=True
+    )
 
-    return render_template("index.html", prediction_text="{}".format(output[0]))
+    if button and input_text:
+        input_text = preprocess(input_text)
+        input_vector = st.session_state.vectorizer.transform([input_text]).toarray()
+        out = st.session_state.model.predict(input_vector.reshape(1, 1, -1))
+        percentage = int(out[0][0] * 100)
+        progress_bar.markdown(
+            progress_bar_value.format(percentage), unsafe_allow_html=True
+        )
+        progress_text.markdown(
+            progress_style.format(percentage), unsafe_allow_html=True
+        )
+
+        # for percentage in range(int(out[0][0] * 100)):
+        #     time.sleep(0.01)
+        #     progress_bar.markdown(
+        #         progress_bar_value.format(percentage), unsafe_allow_html=True
+        #     )
+        # progress_text.markdown(
+        #     progress_style.format(percentage), unsafe_allow_html=True
+        # )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
